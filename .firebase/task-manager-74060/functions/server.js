29ea8eedad9e8b257e4fb80eca8b3128edec2e49
bachdev <nguyenhuybach.mochi@gmail.com>
2026-06-25 16:24:22 +0700

    update

---
+++ .firebase/task-manager-74060/functions/server.js
@@ -0,0 +1,4 @@
+import { onRequest } from 'firebase-functions/v2/https';
+  const server = import('firebase-frameworks');
+  export const ssrtaskmanager74060 = onRequest({"region":"asia-southeast1"}, (req, res) => server.then(it => it.handle(req, res)));
+  
